9d3934b82db639908266a54f044b766c69aec844
Recr <126016150+Recr@users.noreply.github.com>
2024-05-18 18:41:27 -0300

    ex 9

--- modulo-1/lista-exercicios-2/9.js
+++ modulo-1/lista-exercicios-2/9.js
@@ -5,15 +5,45 @@ let employees = Array();
 
 while (!stop) {
     let employeeGender = input("What's the employee gender? Type 'w' for woman and 'm' for man: ").toLowerCase();
-    let salary: input("What's the employee salary? ")
+    let salary = Number(input("What's the employee salary? "));
+    if (salary > 0) {
+        if (employeeGender === 'w') {
+            newEmployee("woman", salary);
+        } else if (employeeGender === 'm') {
+            newEmployee("man", salary);
+        } else {
+            console.log("Invalid Gender!")
+        }
+    } else {
+        console.log("Invalid salary!");
+    }
 
-    console.log(employeeaa)
+    let stopLoop = input("Stop adding employees? 'y' for yes.").toLowerCase();
+    if (stopLoop === 'y') {
+        stop = true;
+    }
+    console.clear();
 }
 
-function newEmployee (employeeGender, employeeSalary) {
+function newEmployee(employeeGender, employeeSalary) {
     const employee = {
         gender: employeeGender,
         salary: employeeSalary
     }
     employees.push(employee);
 }
+
+let womenTotalSalary = 0;
+let menTotalSalary = 0;
+
+employees.forEach((currentEmployee) => {
+    if (currentEmployee.gender === 'woman') {
+        womenTotalSalary += currentEmployee.salary;
+    } else {
+        menTotalSalary += currentEmployee.salary;
+    }
+    }
+)
+
+console.log(`The total amount paid for women was R$ ${womenTotalSalary}.`);
+console.log(`And for the men it was R$ ${menTotalSalary}.`);
