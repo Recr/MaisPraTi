35fc6dbed3883d1eae29bbf7d60efdef3fc322ae
Recr <126016150+Recr@users.noreply.github.com>
2024-06-12 13:38:19 -0300

    ex 46

---
+++ modulo-1/lista-exercicios-2/46.js
@@ -0,0 +1,29 @@
+let vendors = ["Sherlock", "Alan", "Edgar", "Poe", "Watson"];
+
+let sales = Array();
+
+for (let i = 0; i < 10; i++) {
+    let randomVendor = vendors[Math.round(Math.random() * 4)];
+    let randomValue = Math.round(Math.random() * 100);
+    let sale = {
+        name: randomVendor,
+        value: randomValue
+    };
+    sales.push(sale);
+}
+
+function totalSalesPerVendor (salesArray) {
+    let total = {};
+    for (sale in salesArray) {
+        if (salesArray[sale].name in total) {
+            total[salesArray[sale].name] += salesArray[sale].value;
+        } else {
+            total[salesArray[sale].name] = salesArray[sale].value;
+        }
+    }
+    return total;
+}
+console.log(sales)
+console.log(totalSalesPerVendor(sales));
+
+
